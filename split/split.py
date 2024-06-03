48aaf708eeb431bcdbbb85d7a8b0d1c8f8e8f1f5
FourbeFlo <floriane.goyus@gmail.com>
2024-06-03 11:40:02 +0200

    new folder

---
+++ split/split.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Script written by J.-B. Camps
+
+import sys
+import random
+
+if __name__ == '__main__':
+
+    train = open("train.txt", "w")
+    val = open("val.txt", "w")
+    test = open("test.txt", "w")
+
+    random.seed('1214')
+    for i in sys.argv[1:]:
+        monRand = random.random()
+        # adjust the file's name for F.Goy directories 
+        if "../" in i:
+            i = i.replace("../", "")
+        if monRand <= 0.1:
+            test.write(i+"\n")
+        elif 0.1 < monRand <= 0.2:
+            val.write(i+"\n")
+        else:
+            train.write(i+"\n")
+
+    test.close()
+    val.close()
+    train.close()
