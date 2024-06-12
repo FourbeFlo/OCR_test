d338e69ec9e6b82981ebad5426a46193a5d1f192
FourbeFlo <floriane.goyus@gmail.com>
2024-06-12 14:06:52 +0200

    update

--- HTR_calcules/deploy_data.sh
+++ HTR_calcules/deploy_data.sh
@@ -19,7 +19,7 @@ rm -rf FONDUE-LA-PRINT-16
 rm -rf FONDUE-FR-PRINT-16
 rm -rf Lambertus
 #prepare the dataset
-#dataLA remove the copie
+#dataLA remove the copy
 cd ~/data/dataLA || exit
 rm -rf Aretius_1-Tim Bucer_Eph_b Cajetan_Rom Daneau_1-Tim Megander_Eph T_M_Daneau_1_Tim_C1 T_M_Daneau_1_Tim_C2 index_Lambert varia
 #dataLam move the poetry file into dataLA
@@ -27,3 +27,4 @@ cd ~/data/dataLam || exit
 mv data/dataLam/test/13982576* data/dataLA/
 mv data/dataLam/test/13982731* data/dataLA/
 mv data/dataLam/test/2207529* data/dataLA/
+#dataset ready 
