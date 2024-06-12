baa7179715d3d20c461ae499456785edd8afcf35
FourbeFlo <floriane.goyus@gmail.com>
2024-06-12 15:23:23 +0200

    update

--- HTR_calcules/fill_data_folder.sh
+++ HTR_calcules/fill_data_folder.sh
@@ -1,7 +1,9 @@
 #!/bin/env bash
+
 mkdir data_1
 # Move specified directories to data_1
 mv data/dataLA/* data/dataLam/* data/dataFR/* data_1/
+
 # Find and move all files from data_1 subdirectories, renaming to avoid conflicts
 find data_1 -type f | while read -r file; do
   basename=$(basename "$file")
@@ -11,5 +13,6 @@ find data_1 -type f | while read -r file; do
   newname="${filename}_${timestamp}.${extension}"
   mv "$file" "data/$newname"
 done
+
 #clean
 rm -rf data/dataLA data/dataLam data/dataFR data_1
