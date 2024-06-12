8113d4aa248f8b1ba51c04aced0ce01be30243f9
FourbeFlo <floriane.goyus@gmail.com>
2024-06-12 13:29:08 +0200

    new file

--- HTR_calcules/fill_data_folder.sh
+++ HTR_calcules/fill_data_folder.sh
@@ -1,3 +1,15 @@
 #!/bin/env bash
 # mv all file in a master directory named "data"
-find data_1 -type f -exec mv {} data/ \;
+mkdir data_1
+
+# Move specified directories to data_1
+mv data/dataLAM/* data/dataLam/* data/datFR/* data_1/
+
+# Move all files from data_1 back to data, appending a timestamp to avoid conflicts
+for file in data_1/*; do
+  basename=$(basename "$file")
+  timestamp=$(date +%s%N)
+  mv "$file" "data/${basename}_$timestamp"
+done
+
+#rmdir data_1
