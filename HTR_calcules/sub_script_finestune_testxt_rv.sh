e893f295cd3bee9a8a6d86e94e90700a7abfee87
FourbeFlo <floriane.goyus@gmail.com>
2024-06-25 12:07:12 +0200

    Create sub_script_finestune_testxt_rv.sh

---
+++ HTR_calcules/sub_script_finestune_testxt_rv.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+
+# Submission script prepared by S. Solfrini
+
+# Request a GPU resource for a specific job.
+
+#SBATCH --partition=shared-gpu
+#SBATCH --time=01:00:00
+#SBATCH --gpus=1
+#SBATCH --output=kraken-test-2-%j.out
+#SBATCH --mem=12GB
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1,VramPerGpu:2GB
+
+# Load necessary modules.
+
+module load CUDA/11.8.0 GCCcore/11.3.0 Python/3.9.6
+
+# Activate a Python virtual environment from a folder named 'kraken-env' in the user's home directory.
+
+source ~/kraken-env/bin/activate
+
+# Start ketos test. This command will test the selected model (lambertus2.0) and store the results of the test in a file called test_fine-tuning_général.txt.
+
+srun ketos test -d cuda:0 -f alto -m nfd_best.mlmodel -e split/test.txt > test_fine-tuning_générale.txt
+
+# The script can be executed with the following command:
+# sbatch NAME.sh
+# test 1 : 1 hours
+#it's work, but use always this kind of path for the "model" and the test.txt, otherwise it fails.
